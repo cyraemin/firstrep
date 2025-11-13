12c6972a8c2cde176ab3bb4e8da3fc3b9d221ba4
Tamsil Sameera <tamsilsamira@gmail.com>
2025-11-13 18:22:09 +0530

    file handling

---
+++ filehnd.py
@@ -0,0 +1,12 @@
+#File handling in Python refers to working with files—opening, reading, writing, and
+ #closing them using built-in functions. 
+#open()-opens a file
+#p=open(r'path')// writing r gives no issues
+#p.read()//reads the file
+#here are some modes and their description.
+#'r'-reads//file must exist
+#'w'-write//creates file or overwrites.
+#'a'-appends//adds to the end of file
+#'x'-creates a new file, fails if it exists.
+
+
